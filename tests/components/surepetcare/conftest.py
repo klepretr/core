36d118001b1354465c4b90db60512a72adaea438
Chris Talkington <chris@talkingtontech.com>
2020-08-23 21:53:06 -0500

    Update conftest.py

--- tests/components/surepetcare/conftest.py
+++ tests/components/surepetcare/conftest.py
@@ -1,5 +1,11 @@
 """Define fixtures available for all tests."""
+from pytest import fixture
+from surepy import SurePetcare
+
 from homeassistant.helpers.aiohttp_client import async_get_clientsession
+
+from tests.async_mock import AsyncMock
+
 
 @fixture()
 def surepetcare(hass):
@@ -7,7 +13,11 @@ def surepetcare(hass):
     api = SurePetcare(
         "test-username",
         "test-password",
-        async_get_clientsession(hass)
+        hass.loop,
+        async_get_clientsession(hass),
+        api_timeout=1
     )
+
+    api.get_data = AsyncMock(return_value=None)
 
     return api
